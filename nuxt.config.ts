d4bc1729e983a620c631d241a62aaeaf8ae69408
Md Anamul Hasan <53550919+md-anamul-hasan@users.noreply.github.com>
2025-02-02 13:38:34 +0600

    0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  css: ['~/assets/styles/main.css'],
+  modules: ["@nuxt/icon", "nuxt-headlessui", "@nuxt/content"],
+
+  ssr: true,
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+      ],
+    },
+    baseURL: "/", // default
+  },
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+});
