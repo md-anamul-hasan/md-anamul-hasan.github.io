07c0a902a30bfd410465d199d94cda326c5dc9d9
Md Anamul Hasan <53550919+md-anamul-hasan@users.noreply.github.com>
2025-02-02 12:40:09 +0600

    0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite";
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: {
-    enabled: true,
-
-    timeline: {
-      enabled: true,
-    },
-  },
-  css: ["~/assets/styles/main.css"],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
-
-  app: {
-    head: {
-      link: [
-        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
-        { rel: "icon", type: "image/png", href: "/favicon.png" },
-      ],
-    },
-    baseURL: "/", // default
-  },
+  devtools: { enabled: true }
 })
